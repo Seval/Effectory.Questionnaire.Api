4a2072e35f76ab4a08d60efd9176fe3170fbf8de
Seval Mutlu <Seval.MUTLUYILDIRIM@daklapack.com>
2022-12-05 13:14:51 +0100

    Controller first setup

---
+++ Effectory.Questionnaire.Api/Controllers/QuestionnaireController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace effectory.questionnaire.api.Controllers
+{
+    public class QuestionnaireController : ControllerBase
+    {
+        private readonly ILogger<QuestionnaireController> _logger;
+
+        public QuestionnaireController(IMediator mediator, ILogger<QuestionnaireController> logger) : base(mediator)
+        {
+            _logger = logger;
+        }
+
+    }
+}
