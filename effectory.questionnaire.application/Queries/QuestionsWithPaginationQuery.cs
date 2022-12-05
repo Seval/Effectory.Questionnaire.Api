6631b5ec92d4d8c0e9975c221936fb09d3f8037d
Seval Mutlu <Seval.MUTLUYILDIRIM@daklapack.com>
2022-12-05 20:48:27 +0100

    first endpoint

---
+++ effectory.questionnaire.application/Queries/QuestionsWithPaginationQuery.cs
@@ -0,0 +1,55 @@
+﻿using effectory.questionnaire.application.Models;
+using effectory.questionnaire.application.Services;
+using effectory.questionnaire.domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace effectory.questionnaire.application.Queries
+{
+    public class QuestionsWithPaginationQuery : IRequest<List<QuestionModel>>
+    {
+        [Required]
+        public int PageIndex { get; set; }
+
+        [Required]
+        public int PageCount { get; set; }
+    }
+
+    public class QuestionsWithPaginationQueryHandler : IRequestHandler<QuestionsWithPaginationQuery, List<QuestionModel>>
+    {
+        private readonly IQuestionnaireService _questionnaireService;
+
+        public QuestionsWithPaginationQueryHandler(IQuestionnaireService questionnaireService)
+        {
+            _questionnaireService = questionnaireService;
+        }
+        public Task<List<QuestionModel>> Handle(QuestionsWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            List<QuestionModel> questionModels = new List<QuestionModel>();
+            var list = _questionnaireService.List();
+   
+           var questions = list.QuestionnaireItems.Where(x => x.ByItemType(ItemType.Question) && x.ByQuestionId()).Select(q => q.QuestionnaireItems.Select(s =>
+      new QuestionModel
+      {
+          Id = (int)s.QuestionId,
+          Answers = s.QuestionnaireItems.Select(a => new AnswerModel { Id = a.AnswerId, Texts = a.Texts }).ToList(),
+          Texts = s.Texts
+      })).ToList();
+            foreach (var item in questions)
+            {
+                questionModels.AddRange(item);
+            }
+
+            var result = questionModels.Skip(request.PageIndex * request.PageCount).Take(request.PageCount).ToList();
+
+            return Task.FromResult(result);
+
+        }
+    }
+}
---
+++ effectory.questionnaire.infrastructure/IJsonReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace effectory.questionnaire.infrastructure
+{
+    public interface IJsonReader<T>
+    {
+        T Read(string name);
+    }
+}
